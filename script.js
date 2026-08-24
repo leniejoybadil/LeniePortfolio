9fe8de756677dd10589e1d501d2a69927ee26798
Lenie Joy Badil <lenibadil28@gmail.com>
2026-08-24 17:54:34 +0800

    Initial Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+```javascript
+// ========================================
+// LENIE'S PORTFOLIO
+// ========================================
+
+console.log("Lenie's Portfolio is running!");
+
+// More features will be added here later:
+// - Image gallery
+// - Project viewer
+// - Animations
+// - Admin dashboard
+// - Login system
+// - Database
+```
